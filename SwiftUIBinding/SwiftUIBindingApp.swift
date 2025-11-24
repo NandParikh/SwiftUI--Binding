f30ea9216a1d9d768d1f57feade7fb0a3ebf12d2
NandParikh <nand.parikh.apple@gmail.com>
2025-11-24 12:33:49 +0530

    Initial Commit

---
+++ SwiftUIBinding/SwiftUIBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBindingApp.swift
+//  SwiftUIBinding
+//
+//  Created by                     Nand Parikh on 24/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
