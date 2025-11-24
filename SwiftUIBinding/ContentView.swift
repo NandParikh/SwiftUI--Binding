565ac810f7f076eb2e863178b3db400f89e1986c
NandParikh <nand.parikh.apple@gmail.com>
2025-11-24 18:23:47 +0530

    Initial commit

--- SwiftUIBinding/ContentView.swift
+++ SwiftUIBinding/ContentView.swift
@@ -6,18 +6,33 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
+    
+    @State private var isOn : Bool = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Binding Demo")
+                .font(.title)
+                .foregroundStyle(isOn ? .green : .red)
+            
+            ToogleChildView(isOn: $isOn)
         }
         .padding()
     }
 }
+
+struct ToogleChildView : View {
+    @Binding var isOn : Bool
+    
+    var body: some View {
+        Toggle(isOn: $isOn) {
+            Text("Toogle")
+                .font(.title)
+        }
+    }
+}
+
 
 #Preview {
     ContentView()
